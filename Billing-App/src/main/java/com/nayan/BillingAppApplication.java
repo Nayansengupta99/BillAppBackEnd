31ee86a0810cc0ee4b6ff8439687957a777cc85c
Nayansengupta99 <nayansengupta99@gmail.com>
2021-12-29 18:45:54 +0530

    sys env added

--- Billing-App/src/main/java/com/nayan/BillingAppApplication.java
+++ Billing-App/src/main/java/com/nayan/BillingAppApplication.java
@@ -12,24 +12,19 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @SpringBootApplication
 @EnableMongoRepositories
 public class BillingAppApplication {
-	
-
-	
-
 
 	public static void main(String[] args) {
 //		SpringApplication.run(BillingAppApplication.class, args);
-		
-		
-        Properties props = new Properties();
-        props.put("server.port", "8080");
-        props.put("spring.data.mongodb.uri", "mongodb+srv://nayan97:xWHLd4BpdsxdSTnl@cluster0.cgcpm.mongodb.net/bill?retryWrites=true&w=majority");
-        props.put("spring.data.mongodb.databasee", "bill");
-        props.put("spring.jpa.defer-datasource-initialization", "true");
 
+		Properties props = new Properties();
+		String mongoDBUrl = "mongodb+srv://nayan97:" + System.getenv("MONGOPASS")
+				+ "@cluster0.cgcpm.mongodb.net/bill?retryWrites=true&w=majority";
+		props.put("server.port", "8080");
+		props.put("spring.data.mongodb.uri", mongoDBUrl);
+		props.put("spring.data.mongodb.databasee", "bill");
+		props.put("spring.jpa.defer-datasource-initialization", "true");
 
-        new SpringApplicationBuilder(BillingAppApplication.class)
-            .properties(props).run(args);
+		new SpringApplicationBuilder(BillingAppApplication.class).properties(props).run(args);
 	}
 
 }
